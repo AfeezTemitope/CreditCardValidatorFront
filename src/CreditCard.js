0f60434f21949d3583f3fc45629d9dfaaae920ed
unknown <belloafeez28@gmail.com>
2024-10-06 21:55:45 +0100

    added a new update

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -10,7 +10,7 @@ const CardValidator = () => {
 
     const validateCard = async () => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/validate', {
+            const response = await fetch('https://credit-card-validator-seven.vercel.app/api/validate', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ card_number: cardNumber })
